7079be8905ed6f5d7ee423d4b423d55d332fd6a8
kuksun han <davinci1452@naver.com>
2024-01-18 15:38:36 +0900

    safeArea

--- PetshionShow/PetshionShow/View/WishListViewController.swift
+++ PetshionShow/PetshionShow/View/WishListViewController.swift
@@ -8,18 +8,7 @@
 import UIKit
 
 class WishListViewController: BaseViewController {
-    
-    // 메인레이블
-    let labelMain: UILabel = {
-        let label = UILabel()
-        label.text = "장바구니"
-        // label.backgroundColor = .systemGray5
-        label.textAlignment = .center
-        label.font = .boldSystemFont(ofSize: 20)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
+
     // 스크롤 뷰
     lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
@@ -48,7 +37,7 @@ class WishListViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.title = "장바구니"
     }
     
     override func buildInterface() {
@@ -61,7 +50,6 @@ class WishListViewController: BaseViewController {
     }
     
     func buildAddView() {
-        view.addSubview(labelMain)
         view.addSubview(scrollView)
         scrollView.addSubview(imageView)
         scrollView.addSubview(productButton)
@@ -69,18 +57,12 @@ class WishListViewController: BaseViewController {
     }
     
     func buildAutoLayout() {
-        // 메인 레이블 레이아웃
-        NSLayoutConstraint.activate([
-            labelMain.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
-            labelMain.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
-            labelMain.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            labelMain.heightAnchor.constraint(equalToConstant: 40)
-        ])
-        
+ 
+       
         // 스크롤 뷰 레이아웃
         NSLayoutConstraint.activate([
             scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
-            scrollView.topAnchor.constraint(equalTo: labelMain.bottomAnchor, constant: 4),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 4),
             scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
             scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
         ])
